7b66bc3ccb09334703ea3acd2e148b341785079d
rianwp <rianwp2015@gmail.com>
2023-06-11 22:00:51 +0700

    fix logo login

--- components/login/LoginHeader.tsx
+++ components/login/LoginHeader.tsx
@@ -3,8 +3,8 @@ import Image from "next/image"
 const LoginHeader = () => {
   return (
     <div className="w-full flex flex-col items-center justify-center mb-10">
-      <Image className="w-12 h-12 mb-2.5" src="/logo.png" alt="Logo" width={48} height={48}/>
-      <h1 className="text-3xl font-bold bg-gradient-to-r from-blue-600 via-sky-500 to-emerald-400 inline-block text-transparent bg-clip-text text-center">Login ke Teman Soal</h1>
+      <Image className="w-10 h-10 mb-2.5" src="/logo.png" alt="Logo" width={40} height={40}/>
+      <h1 className="text-2xl font-bold bg-gradient-to-r from-blue-600 via-sky-500 to-emerald-400 inline-block text-transparent bg-clip-text text-center">Login ke Teman Soal</h1>
     </div>
   )
 }
