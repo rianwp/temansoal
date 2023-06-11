b14700d43067e65661f11284a0cb7c91ac27aa88
rianwp <rianwp2015@gmail.com>
2023-06-11 14:42:45 +0700

    add loading login button

--- components/login/FacebookLoginButton.tsx
+++ components/login/FacebookLoginButton.tsx
@@ -3,12 +3,24 @@
 import { signIn } from "next-auth/react"
 import { ImFacebook } from "react-icons/im"
 import { Button } from "../ui/button"
-
+import { useState } from "react"
+import { Loader2 } from "lucide-react"
 
 const FacebookLoginButton = () => {
+  const [isClicked, setIsClicked] = useState<Boolean>(false)
+
+  const buttonClick = () => {
+    setIsClicked(true)
+    signIn("facebook")
+  }
+
   return (
-    <Button variant="outline" size="lg" className="flex flex-row space-x-2 justify-start items-center w-full" onClick={() => signIn("facebook")}>
-      <ImFacebook className="w-8 h-8 text-[#1877F2]"/>
+    <Button variant="outline" size="lg" className="flex flex-row space-x-2 justify-start items-center w-full" onClick={buttonClick}>
+      {!isClicked ?
+        <ImFacebook className="w-8 h-8 text-[#1877F2]"/>
+        :
+        <Loader2 className="h-8 w-8 animate-spin text-gray-400" />
+      }
       <p className="text-gray-400">Login dengan Facebook</p>
     </Button>
   )
--- components/login/GoogleLoginButton.tsx
+++ components/login/GoogleLoginButton.tsx
@@ -3,11 +3,23 @@
 import { signIn } from "next-auth/react"
 import { FcGoogle} from "react-icons/fc"
 import { Button } from "../ui/button"
+import { Loader2 } from "lucide-react"
+import { useState } from "react"
 
 const GoogleLoginButton = () => {
+  const [isClicked, setIsClicked] = useState<Boolean>(false)
+
+  const buttonClick = () => {
+    setIsClicked(true)
+    signIn("google")
+  }
   return (
-    <Button variant="outline" size="lg" className="flex flex-row space-x-2 justify-start items-center w-full" onClick={() => signIn("google")}>
-      <FcGoogle className="w-8 h-8"/>
+    <Button variant="outline" size="lg" className="flex flex-row space-x-2 justify-start items-center w-full" onClick={buttonClick}>
+      {!isClicked ?
+        <FcGoogle className="w-8 h-8"/>
+        :
+        <Loader2 className="h-8 w-8 animate-spin text-gray-400" />
+      }
       <p className="text-gray-400">Login dengan Google</p>
     </Button>
   )
