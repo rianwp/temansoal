b29fc69f21bef13bca9da8ae7104dace56718f5f
rianwp <rianwp2015@gmail.com>
2023-06-14 18:35:11 +0700

    add button and text to hero

--- components/main/hero/Hero.tsx
+++ components/main/hero/Hero.tsx
@@ -1,9 +1,15 @@
+import { Button } from "@/components/ui/button"
 import HeroText from "./HeroText"
 
 const Hero = () => {
   return (
     <div className="w-full landscape:h-screen hero-responsive bg-gradient-to-r from-blue-600 via-sky-500 to-emerald-400 -mt-16 pt-16 flex flex-col justify-center p-3 lg:px-10">
-      <HeroText/>
+      <div className="w-1/2">
+        <HeroText/>
+        <div className="flex flex-row mt-10">
+          <Button size="lg">Coba Sekarang</Button>
+        </div>
+      </div>
     </div>
   )
 }
