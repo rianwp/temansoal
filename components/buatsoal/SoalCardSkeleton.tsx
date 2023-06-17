b79f423a366ddd9cb94d7410e85d8954451c5346
rianwp <rianwp2015@gmail.com>
2023-06-17 08:40:03 +0700

    fix layout

--- components/buatsoal/SoalCardSkeleton.tsx
+++ components/buatsoal/SoalCardSkeleton.tsx
@@ -11,15 +11,15 @@ const SoalCardSkeleton = () => {
     <Card className="w-full p-4">
       <h2 className="flex flex-row items-center">
         <Skeleton className="rounded-full flex justify-center items-center h-5 w-5"/>
-        <Skeleton className="ml-1 h-6 w-full mb-1"/>
+        <Skeleton className="ml-2 h-6 w-full mb-2"/>
       </h2>
       <div className="pl-6 w-full">
         {haveOptions ?
-          <div className="flex flex-col space-y-1 mb-2">
+          <div className="flex flex-col space-y-2 mb-2">
             {[...Array(5)].map((item, index) => (
               <p key={index} className="flex flex-row">
-                <Skeleton className="h-4 w-10"/>
-                <Skeleton className="h-4 w-full"/>
+                <Skeleton className="h-4 w-4"/>
+                <Skeleton className="ml-2 h-4 w-full"/>
               </p>
             ))}
           </div>
@@ -27,7 +27,6 @@ const SoalCardSkeleton = () => {
           null
         }
         <div className="flex flex-col">
-          <Skeleton className="h-4 w-full mb-1"/>
           <Skeleton className="h-4 w-full"/>
         </div>
       </div>
