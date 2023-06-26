f9ad15fffbf8553d485369d652d7bbce7342ac4a
rianwp <rianwp2015@gmail.com>
2023-06-27 06:14:10 +0700

    fix space

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -2,9 +2,9 @@ import Link from "next/link"
 
 const Footer = () => {
   return (
-    <div className="border-t border-gray-200 flex flex-row justify-between p-4 md:px-10">
+    <div className="border-t border-gray-200 flex flex-row justify-between p-4 md:px-10 text-sm">
       <p className="text-gray-400">&#169; 2023 Teman Soal</p>
-      <div className="flex flex-row space-x-4">
+      <div className="flex flex-row md:space-x-4 space-x-2">
         <Link href="/termsofservice" className="text-gray-400 hover:text-gray-500 transition duration-300">Terms of Service</Link>
         <Link href="/privacypolicy" className="text-gray-400 hover:text-gray-500 transition duration-300">Privacy Policy</Link>
       </div>
