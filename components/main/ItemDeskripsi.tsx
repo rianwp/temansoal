6630a399e654eccade31d5e9bab7ea2b5494d5a9
rianwp <rianwp2015@gmail.com>
2023-06-27 23:30:06 +0700

    adding itemdeskripsi

---
+++ components/main/ItemDeskripsi.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import { motion } from "framer-motion"
+
+interface ItemDeskripsiProps {
+  icon: React.ReactNode,
+  judul: string,
+  deskripsi: string
+}
+
+const ItemDeskripsi = ({icon, judul, deskripsi}: ItemDeskripsiProps) => {
+  const animate = {
+    initial: { opacity: 0, x: 40},
+    in: { opacity: 1, x: 0}
+  }
+  return (
+    <motion.div
+      variants={animate}
+      whileInView="in"
+      initial="initial"
+      viewport={{ once: true }}
+      className="flex flex-col space-y-2 w-full"
+    >
+      <div className="flex flex-row space-x-2 items-center">
+        {icon}
+        <p className="text-base font-semibold">{judul}</p>
+      </div>
+      <div className="w-full text-left text-sm">
+        {deskripsi}
+      </div>
+    </motion.div>
+  )
+}
+
+export default ItemDeskripsi
