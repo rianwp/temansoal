5da27396f0e912c7bbbc1ddba06320dd5ca245a8
rianwp <rianwp2015@gmail.com>
2023-06-28 22:20:03 +0700

    centering image

--- components/main/hero/HeroImage.tsx
+++ components/main/hero/HeroImage.tsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 const HeroImage = () => {
   return (
-    <div className="md:w-1/2 w-0 md:block hidden">
+    <div className="md:w-1/2 w-0 md:flex hidden justify-center items-center">
       <Image className="w-11/12 object-center object-contain" src="/hero.png" alt="Hero Book" width={1920} height={1920}/>
     </div>
   )
