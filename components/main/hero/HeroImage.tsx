96dbb5e0d057c1f271cf497ac54b634c69b6638d
rianwp <rianwp2015@gmail.com>
2023-06-28 22:22:39 +0700

    fix image

--- components/main/hero/HeroImage.tsx
+++ components/main/hero/HeroImage.tsx
@@ -2,8 +2,8 @@ import Image from "next/image"
 
 const HeroImage = () => {
   return (
-    <div className="md:w-1/2 w-0 md:flex hidden justify-center items-center">
-      <Image className="w-11/12 object-center object-contain" src="/hero.png" alt="Hero Book" width={1920} height={1920}/>
+    <div className="md:w-1/2 w-0 md:block hidden">
+      <Image className="w-full object-center object-contain" src="/hero.png" alt="Hero Book" width={1920} height={1920}/>
     </div>
   )
 }
