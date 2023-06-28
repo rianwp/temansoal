d6cb947675cf4db07d0ef5fa846a046653a4f938
rianwp <rianwp2015@gmail.com>
2023-06-28 22:05:18 +0700

    footer responsive

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 const Footer = () => {
   return (
-    <div className="border-t border-gray-200 flex md:flex-row flex-col space-y-1 md:space-y-0 md md:justify-between p-4 md:px-10 text-sm">
+    <div className="border-t border-gray-200 flex sm:flex-row flex-col space-y-1 sm:space-y-0 sm:justify-between p-4 sm:px-10 text-sm">
       <p className="text-gray-400">&#169; 2023 Teman Soal</p>
       <div className="flex flex-row md:space-x-4 space-x-2">
         <Link href="/termsofservice" className="text-gray-400 hover:text-gray-500 transition duration-300">Terms of Service</Link>
