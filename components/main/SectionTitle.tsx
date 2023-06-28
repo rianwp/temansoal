53af720ff2c8f0aec98fd7dea332872b678a9df5
rianwp <rianwp2015@gmail.com>
2023-06-28 21:55:39 +0700

    edit section padding

--- components/main/SectionTitle.tsx
+++ components/main/SectionTitle.tsx
@@ -4,7 +4,7 @@ interface SectionTitleProps {
 
 const SectionTitle = ({children}: SectionTitleProps) => {
   return (
-    <h1 className="text-3xl font-bold mx-auto mb-10 text-center">{children}</h1>
+    <h1 className="text-3xl font-bold mx-auto mb-10 text-center px-4 md:px-10">{children}</h1>
   )
 }
 
