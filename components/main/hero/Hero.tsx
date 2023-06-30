9ef32bd24a779e4becd69968665ac2998158131c
rianwp <rianwp2015@gmail.com>
2023-06-30 10:25:32 +0700

    fixing responsive

--- components/main/hero/Hero.tsx
+++ components/main/hero/Hero.tsx
@@ -6,7 +6,7 @@ import OpenaiLogo from "./OpenaiLogo"
 
 const Hero = () => {
   return (
-    <div className="w-full landscape:h-screen hero-responsive bg-gradient-to-r from-blue-600 via-sky-500 to-emerald-400 -mt-16 pt-16 flex flex-row justify-center md:justify-start items-center p-3 md:px-10">
+    <div className="w-full landscape:min-h-screen hero-responsive bg-gradient-to-r from-blue-600 via-sky-500 to-emerald-400 -mt-16 pt-16 flex flex-row justify-center md:justify-start items-center p-3 md:px-10">
       <div className="md:w-1/2 w-full flex flex-col md:items-start items-center">
         <HeroText/>
         <HeroCounter/>
