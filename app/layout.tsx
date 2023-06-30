dffbc75fd8822a726d50d2ff5ee27afafe139076
rianwp <rianwp2015@gmail.com>
2023-06-30 11:14:04 +0700

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import Provider from "@/components/Provider"
 import "./globals.css"
 import { Roboto} from "next/font/google"
 import { Toaster } from "@/components/ui/toaster"
+import { Analytics } from "@vercel/analytics/react"
 
 const font = Roboto({ subsets: ["latin"], weight: ["100", "300", "400", "500", "700", "900"] })
 
@@ -20,6 +21,7 @@ const RootLayout = ({children}: RootLayoutProps) => {
       <html lang="en">
         <body className={`${font.className} overflow-x-hidden`}>
           {children}
+          <Analytics />
           <Toaster/>
         </body>
       </html>
