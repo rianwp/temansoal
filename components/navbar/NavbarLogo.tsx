15320945e8232d6a587ae24bf59447baf31903f0
rianwp <rianwp2015@gmail.com>
2023-07-04 18:56:30 +0700

    fix logo

--- components/navbar/NavbarLogo.tsx
+++ components/navbar/NavbarLogo.tsx
@@ -7,13 +7,12 @@ interface NavbarLogoProps {
 
 const NavbarLogo = ({onTop}: NavbarLogoProps) => {
   const textStyle = onTop ? "text-white" : "text-sky-600"
+  const whiteLogo = onTop ? "block" : "hidden"
+  const colorLogo = onTop ? "hidden" : "block"
   return (
     <Link href="/" className="flex flex-row justify-between items-center space-x-2">
-      {onTop ?
-        <Image src="/logowhite.png" alt="Logo" width={32} height={24}/>
-        :
-        <Image src="/logo.png" alt="Logo" width={32} height={24}/>
-      }
+        <Image className={whiteLogo} src="/logowhite.png" alt="Logo" width={32} height={24}/>
+        <Image className={colorLogo} src="/logo.png" alt="Logo" width={32} height={24}/>
       <p className={`${textStyle} font-medium text-base`}>Teman Soal</p>
     </Link>
   )
